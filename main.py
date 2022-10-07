70eb0e9f4e5392babac6d4caa4710d9fe5018ed2
RhoAias-Poro <85359791+RhoAias-Poro@users.noreply.github.com>
2022-10-07 08:27:11 +0700

    update

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ import json, datetime, asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="/",
+                   intents=intents)  #set prefix to use command
 
 
-@bot.command(name="hi")
+@bot.command(name="hi")  # if user enter /hi then print out hello
 async def SendMessage(ctx):
   await ctx.send('Hello!')
 
 
 @bot.command(name="pussy")
 async def Cat(ctx):
-  response = requests.get(  # send requests to the api and response now a json
-    "https://api.thecatapi.com/v1/images/search?api_key=live_RbZGAHDXr6v3pV32pvbUe9yBEG6hH20VEtuJyDX0SsFJe3nVesTP00Z8ZLtrHlgz"
-  )
+  api = os.environ['API']
+  # send requests to the api and response now a json
+  response = requests.get(api)
   image_link = response.json()[0]  # like a list and need to choose index
   await ctx.send(image_link["url"])  # print the url
 
@@ -30,7 +31,8 @@ async def five_minutes_images():
     wait_time = (then - now).total_seconds()
     await asyncio.sleep(wait_time)
 
-    channel = bot.get_channel(1027525240055676969)
+    ID = os.environ['channelID']  # return string obj
+    channel = bot.get_channel(int(ID))
     await Cat(channel)
 
 
